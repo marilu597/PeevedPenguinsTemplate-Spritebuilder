ba6e29831e984ede54ac40365af815b6dd3b9e16
Maria Luisa <marialuisa@Marias-MacBook-Pro.local>
2015-01-28 00:22:14 -0800

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Maria Luisa on 1/28/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode
+
+@end
---
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -0,0 +1,42 @@
+//
+//  Gameplay.m
+//  PeevedPenguins
+//
+//  Created by Maria Luisa on 1/28/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "Gameplay.h"
+
+@implementation Gameplay {
+    CCPhysicsNode *_physicsNode;
+    CCNode *_catapultArm;
+}
+
+// Is called when CCB file has completed loading
+-(void) didLoadFromCCB {
+    // Tell this scene to accept touches
+    self.userInteractionEnabled = TRUE;
+}
+
+// Called on every touch in this scene
+-(void) touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event {
+    [self launchPenguin];
+}
+
+-(void) launchPenguin {
+    // Loads the Penguin.ccb we have set up in Spritebuilder
+    CCNode *penguin = [CCBReader load:@"Penguin"];
+    // Position the penguin at the bowl of the catapult
+    penguin.position = ccpAdd(_catapultArm.position, ccp(16, 50));
+    
+    // Add the penguin to the physicsNode of this scene (because it has physics enabled)
+    [_physicsNode addChild:penguin];
+    
+    // Manually create & apply a force to launch the penguin
+    CGPoint launchDirection = ccp(1, 0);
+    CGPoint force = ccpMult(launchDirection, 8000);
+    [penguin.physicsBody applyForce:force];
+}
+
+@end
