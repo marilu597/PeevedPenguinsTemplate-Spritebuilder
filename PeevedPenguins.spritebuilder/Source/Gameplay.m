5bb3e27167e6d0140cac8210579682ccee42cfb0
Maria Luisa <marialuisa@Marias-MacBook-Pro.local>
2015-02-04 01:11:48 -0800

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -54,7 +54,7 @@ static const float MIN_SPEED = 5.f;
     _physicsNode.collisionDelegate = self;
     
     // Visualize physics bodies & joints
-    //_physicsNode.debugDraw = TRUE;
+    _physicsNode.debugDraw = TRUE;
 }
 
 -(void) update:(CCTime)delta {
